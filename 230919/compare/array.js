7ca4785af81c02b3e06e384590ce282c6a634a3f
sunday97 <a01029243700@gmail.com>
2023-09-19 11:20:02 +0900

    array

---
+++ 230919/compare/array.js
@@ -0,0 +1,23 @@
+// array.js
+// console.log('array.js running...')
+
+// 포켓몬
+const pokemon = [`피카추`,`이브이`,`라이츄`,`파이리`,`꼬부기`]
+// push는 배열 마지막에 요소를 추가
+pokemon.push(`망냐뇽`)
+
+// 포켓몬 배열에 이상해씨를 추가해주세요
+pokemon.push(`이상해씨`)
+
+// 배열의 '마지막 요소를 제거'
+pokemon.pop();
+
+console.log(pokemon)
+
+// 배열.length : 배열의 길이를 알아 보는 방법
+console.log(pokemon.length)
+// 배열의 요소를 셀 때는 0부터 시작합니다.
+console.log(pokemon[0]) //피카추
+//파이리를 출력해주세요
+console.log(pokemon[3]) //파이리
+
