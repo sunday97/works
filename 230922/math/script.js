c1fb2eabaa7b60dc078e313eb86f99b2b07b19c4
sunday97 <a01029243700@gmail.com>
2023-09-22 17:27:59 +0900

    Math

---
+++ 230922/math/script.js
@@ -0,0 +1,53 @@
+console.log(`math`)
+console.log(`--PI--`)
+// PI
+console.log(Math.PI)
+
+console.log(`--절대값--`)
+
+// 절대값
+console.log(Math.abs(-1))
+console.log(Math.abs(1))
+console.log(Math.abs(`1`))
+
+console.log(`--반올림--`)
+
+// 반올림
+console.log(Math.round(1.4))
+console.log(Math.round(1.5))
+
+console.log(`--올림--`)
+
+// 올림
+console.log(Math.ceil(1.2))
+console.log(Math.ceil(-1.2))
+
+console.log(`--버림--`)
+
+// 버림
+console.log(Math.floor(1.9999999999999))
+console.log(Math.floor(-1.9999999999999))
+
+console.log(`--랜덤--`)
+
+// random : 0부터 0.99999999999999999....
+console.log(Math.random())
+console.log(Math.random()+1)
+// Math.random() *(최댓값 - 최솟값 + 1) + 최솟값;
+
+
+
+let max = 45;
+let min = 1;
+const random = Math.random() * (max - min + 1) + min
+console.log(random)
+console.log(Math.floor(random))
+// random = Math.floor(Math.random() * (max - min + 1) + min)
+
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+// 최소, 최대 값을 받아 해당 범위만큼 난수를 생성하는 함수
+const randomGenerator = (max, min) => {
+    return console.log(Math.floor(Math.random() * (max - min + 1) + min))
+}
+randomGenerator(1,77)
